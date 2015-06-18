004846720a2e07e4ee6c71bb732de7229bb26d74
Tr1cks <matthew.james.mccarthy@gmail.com>
2015-06-18 19:13:00 -0400

    updates to run inkscape scripts on mac

--- art/convert.py
+++ art/convert.py
@@ -1,10 +1,17 @@
 import os
 #sos.system("inkscape blue.svg --export-png=../android/assets/640/blue_test.png -w600")
+mac_ink = "/Applications/Inkscape.app/Contents/Resources/bin/inkscape "
+prefix = "/users/matt/documents/dev/libgdx/box_puzzle/"
 
-sizes = [480,640,960]
-images = [['blue.svg', 'blue_test.png', 48]]
+sizes = ['480','640','960']
+images = [
+	['blue.svg', 'blue_test.png', ['48','64','96']],
+	['about_text.svg', 'about_text.png', ['380','570','760']],
+]
 
 for x in images:
-	print x
+	for i, y in enumerate(sizes):
+		command = mac_ink + prefix + "art/" + x[0] + " --export-png="+ prefix+"/android/assets/" + y + "/" + x[1] + " -w"+x[2][i]
+		os.system(command)
 
 #command = "inkscape " + "blue.svg" + "--export-png=../android/assets/" + "640/" + "blue_test.png" + "-w600"
